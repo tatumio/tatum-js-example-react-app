618157f83f301e9f9dff17ab6a2897fa72c9f94d
Jan Dočekal <jan.docekal@tatum.io>
2022-10-20 15:35:00 +0200

    feat: add broadcast example

--- src/pages/wallet/transactions.tsx
+++ src/pages/wallet/transactions.tsx
@@ -22,12 +22,17 @@ export default function WalletTransactionsPage() {
   const [transferLoading, setTransferLoading] = React.useState(false);
   const [transferResponse, setTransferResponse] = React.useState('');
 
+  const [broadcastLoading, setBroadcastLoading] = React.useState(false);
+  const [broadcastResponse, setBroadcastResponse] = React.useState('');
+
   const [formData, setFormData] = React.useState({
     to: '',
     fromPrivateKey: '',
     amount: '',
     value: 0,
     from: '',
+    txData: '',
+    signatureId: '',
   });
 
   const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
@@ -116,12 +121,69 @@ export default function WalletTransactionsPage() {
         typeof response !== 'string' ? JSON.stringify(response) : response
       );
     setTransferLoading(false);
-    toast.success('Block received successfuly!');
+    toast.success('TX sent successfuly!');
   };
 
-  //const broadcastTx = async (e: React.FormEvent<HTMLFormElement>) => {
-  // @todo
-  //};
+  const broadcastTx = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setBroadcastLoading(true);
+
+    let response;
+
+    try {
+      switch (selectedChain) {
+        case Chains.Ethereum:
+          response = await TatumEthSDK(SDKOptions).blockchain.broadcast({
+            txData: formData.txData,
+            signatureId: formData.signatureId
+              ? formData.signatureId
+              : undefined,
+          });
+          break;
+        case Chains.Polygon:
+          response = await TatumPolygonSDK(SDKOptions).blockchain.broadcast({
+            txData: formData.txData,
+            signatureId: formData.signatureId
+              ? formData.signatureId
+              : undefined,
+          });
+          break;
+        case Chains.Bitcoin:
+          response = await TatumBtcSDK(SDKOptions).blockchain.broadcast({
+            txData: formData.txData,
+            signatureId: formData.signatureId
+              ? formData.signatureId
+              : undefined,
+          });
+          break;
+        case Chains.Solana:
+          // todo
+          return;
+        case Chains.Tron:
+          response = TatumTronSDK(SDKOptions).blockchain.broadcast({
+            txData: formData.txData,
+          });
+          break;
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+        if (JSON.parse(e?.message)?.message) {
+          toast.error(JSON.parse(e.message).message);
+        } else {
+          toast.error(e?.message);
+        }
+      }
+      setBroadcastLoading(false);
+      return;
+    }
+
+    if (response)
+      setBroadcastResponse(
+        typeof response !== 'string' ? JSON.stringify(response) : response
+      );
+    setBroadcastLoading(false);
+    toast.success('Broadcasted successfuly!');
+  };
 
   //const transferERC = async (e: React.FormEvent<HTMLFormElement>) => {
   // @todo
@@ -219,6 +281,54 @@ export default function WalletTransactionsPage() {
             {transferResponse ? `Block: ${transferResponse}` : ''}
           </div>
         </div>
+
+        {selectedChain !== Chains.Solana && (
+          <div className='layout mb-8 flex flex-col items-baseline justify-start text-left text-black'>
+            <h4>Broadcast transaction</h4>
+            <form
+              onSubmit={(e) => broadcastTx(e)}
+              className='mt-2 flex w-full flex-wrap rounded-md bg-neutral-100 pt-4 pb-3'
+            >
+              <div className='mb-2 flex w-full flex-row'>
+                <div className='w-full px-3'>
+                  <Input
+                    required
+                    inputType='text'
+                    placeholder='TX DATA'
+                    name='txData'
+                    onChange={(e) => handleInputChange(e)}
+                  />
+                </div>
+              </div>
+              {selectedChain !== Chains.Tron && (
+                <div className='mb-2 flex w-full flex-row'>
+                  <div className='w-full px-3'>
+                    <Input
+                      inputType='text'
+                      placeholder='SIGNATURE ID'
+                      name='signatureId'
+                      onChange={(e) => handleInputChange(e)}
+                    />
+                  </div>
+                </div>
+              )}
+              <div className='flex w-full flex-row items-center justify-start px-3'>
+                <Button isLoading={broadcastLoading} type='submit'>
+                  Broadcast
+                </Button>
+              </div>
+            </form>
+
+            <div
+              className={clsxm(
+                'flex w-full items-center rounded-bl-md rounded-br-md bg-zinc-900 px-4 py-2 text-white transition-max-height duration-300 ease-in-out',
+                broadcastResponse ? 'max-h-50' : 'max-h-0'
+              )}
+            >
+              {broadcastResponse}
+            </div>
+          </div>
+        )}
       </main>
     </Layout>
   );
