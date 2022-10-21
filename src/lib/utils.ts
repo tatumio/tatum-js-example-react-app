c148f83d1fcbb93b63b47078035e1d2e1c652c47
Jan Dočekal <hello@jdckl.dev>
2022-10-21 12:20:18 +0200

    chore: allow utils type ignore

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,10 @@
 import React, { Dispatch } from 'react';
 
-// eslint-disable-next-line
 export const handleInputChange = (
   e: React.FormEvent<HTMLInputElement>,
+  // eslint-disable-next-line
   hook: Dispatch<any>,
+  // eslint-disable-next-line
   data: any
 ) => {
   hook({ ...data, [e.target.name]: (e.target as HTMLInputElement).value });
